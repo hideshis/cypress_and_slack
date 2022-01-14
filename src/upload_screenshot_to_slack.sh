7905244159289cec4e07079b2c678137bb04eeb2
hideshis <hideshis1990@gmail.com>
2022-01-15 05:34:39 +0900

    Update upload_screenshot_to_slack.sh

--- src/upload_screenshot_to_slack.sh
+++ src/upload_screenshot_to_slack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PATH_TO_FILE=./cypress/screenshots/sample/sample.spec.js/sample_screenshot.png
-CHANNEL_ID=C0238EUP7F0
+CHANNEL_ID=hogehoge
 USER_OAUTH_TOKEN=xoxp-hogehoge
 
 curl -F file=@${PATH_TO_FILE} -F channels=${CHANNEL_ID} -H "Authorization: Bearer ${USER_OAUTH_TOKEN}" https://slack.com/api/files.upload
